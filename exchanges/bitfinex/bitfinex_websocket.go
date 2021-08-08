ac1a77eb5f22de248f93ab2929c2d67da2d39215
yliankuo <yliankuo@yahoo.com>
2021-08-08 19:29:40 -0400

    Revert "Attempt to fix bitfinex websocket"
    This reverts commit 6b4793ec71abb88fe74cf999dc5b2706944b2efd.

--- exchanges/bitfinex/bitfinex_websocket.go
+++ exchanges/bitfinex/bitfinex_websocket.go
@@ -459,8 +459,11 @@ func (b *Bitfinex) wsHandleData(respRaw []byte) error {
 					}
 				}
 			case 3:
-				if d[1].(string) != wsFundingTradeUpdate &&
-					d[1].(string) != wsTradeExecutionUpdate {
+				if d[1].(string) == wsTradeExecutionUpdate ||
+					d[1].(string) == wsFundingTradeUpdate {
+					// "(f)te - trade executed" && "(f)tu - trade updated"
+					// contain the same amount of data
+					// "(f)te" gets sent first so we can drop "(f)tu"
 					return nil
 				}
 				data := d[2].([]interface{})
